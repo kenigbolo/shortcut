8c960f6a65c137ed5b45f6e4702a57eb049efc27
kenigbolo <kenigbol@ut.ee>
2016-07-13 17:46:17 +0300

    Update the attributes for Cargo and Ship Migrations

--- db/migrate/20160713124402_create_ships.rb
+++ db/migrate/20160713124402_create_ships.rb
@@ -1,7 +1,14 @@
 class CreateShips < ActiveRecord::Migration[5.0]
   def change
     create_table :ships do |t|
+      t.references :ship_owner, index: true
 
+      t.string	:present_port
+      t.string	:destination_port
+      t.decimal	:gross_tonne
+      t.decimal	:dead_weight
+      t.integer	:imo_number
+      t.string	:ship_type
       t.timestamps
     end
   end
--- db/migrate/20160713124408_create_cargos.rb
+++ db/migrate/20160713124408_create_cargos.rb
@@ -1,7 +1,15 @@
 class CreateCargos < ActiveRecord::Migration[5.0]
   def change
     create_table :cargos do |t|
-
+      t.references :cargo_owner, index: true
+      
+      t.decimal	:weight
+      t.decimal	:volume
+      t.decimal	:density
+      t.string	:cargo_type
+      t.string	:present_port
+      t.string	:destination_port
+      t.date	:arrival_date	
       t.timestamps
     end
   end
